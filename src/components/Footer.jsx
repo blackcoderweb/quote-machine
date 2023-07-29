9e9e7f85fa2327e215ca2e604d5095fb8a72e6b4
BlackCoder <darielconde@gmail.com>
2023-07-28 18:04:48 -0600

    Updates

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-
-
-export const Footer = () => {
-  return (
-    <div id="footer">developed by <a href="https://blackcoderweb.github.io/personal-portfolio/" target="_blank" rel="noopener noreferrer">BlackCoder</a></div>
-  )
-}
